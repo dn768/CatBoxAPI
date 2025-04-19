6c8b5d69e39d494e9f857f4b10c0b946dcdc521d
David Novak <david.novak@flaggerforce.com>
2025-04-19 14:16:22 -0400

    4: Add List endpoint to get a list of all Cat Profiles, filterable and sortable by one starting field each

--- CatBoxAPI/Controllers/CatProfileController.cs
+++ CatBoxAPI/Controllers/CatProfileController.cs
@@ -105,4 +105,57 @@ public class CatProfileController(ICatProfileService catProfileService) : Contro
         }
         return Ok();
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CatProfileListItemDTO>>> List([FromQuery] string? filter, [FromQuery] string? sort, IWebHostEnvironment webHostEnvironment)
+    {
+        BoxSize? filterBySize = null;
+
+        if (!string.IsNullOrEmpty(filter?.Trim()))
+        {
+            // Accept any non-letter delimiter of field and value
+            // (Ex: BoxSize:Small or boxsize+large or boxSize%20=%20extraLarge)
+            var filterField = new string([.. filter.Trim().ToCharArray().TakeWhile(char.IsAsciiLetter)]);
+            var filterValue = new string([.. filter.Trim().ToCharArray().Reverse().TakeWhile(char.IsAsciiLetter).Reverse()]); //TODO: Consider a more efficient way to get this
+
+            if (filterField.Trim().Equals("boxsize", StringComparison.CurrentCultureIgnoreCase))
+            {
+                if (Enum.TryParse(filterValue, true, out BoxSize boxSize))
+                    filterBySize = boxSize;
+                else
+                    return BadRequest($"Filter by BoxSize requires one of these values: {string.Join(", ", Enum.GetValues<BoxSize>())}");
+            }
+            else
+                return BadRequest("Filter feature only supports BoxSize");
+        }
+
+
+        // Alternative approach is used here to get the field name and value, just to keep things interesting
+        // TODO: Consider using a consistent and extensible approach instead
+        bool? sortByAge = null;
+        if (!string.IsNullOrEmpty(sort?.Trim()))
+        {
+            if (!sort.Trim().StartsWith("age", StringComparison.CurrentCultureIgnoreCase))
+                return BadRequest($"Sorting feature is provided only for age.");
+
+            // TODO: Consider using a different data type to represent sorting preference, since false may imply unsorted rather than sorted in descending order
+            if (sort.Trim().EndsWith("desc", StringComparison.CurrentCultureIgnoreCase))
+                sortByAge = false;
+            else
+                sortByAge = true;
+        }
+
+
+        IEnumerable<CatProfileListItemDTO> list;
+        try
+        {
+            list = await _catProfileService.GetCatProfileListAsync(filterBySize, sortByAge);
+        }
+        catch (Exception ex)
+        {
+            string errorMessage = webHostEnvironment.IsDevelopment() ? $"\r\n{ex.Message}\r\n{ex.InnerException}" : "";
+            return Problem($"Request for cat profile list encountered an error.{errorMessage}");
+        }
+        return Ok(list);
+    }
 }
--- CatBoxAPI/Services/ICatProfileService.cs
+++ CatBoxAPI/Services/ICatProfileService.cs
@@ -1,4 +1,5 @@
-﻿using CatBoxAPI.Models.CatProfile;
+﻿using CatBoxAPI.Enums;
+using CatBoxAPI.Models.CatProfile;
 
 namespace CatBoxAPI.Services;
 
@@ -7,4 +8,5 @@ public interface ICatProfileService
     Task<Guid> CreateCatProfileAsync(CatProfileCreationDTO catProfile);
     Task UpdateCatProfileAsync(CatProfileEditDTO catProfile);
     Task DeleteCatProfileAsync(Guid id);
+    Task<IEnumerable<CatProfileListItemDTO>> GetCatProfileListAsync(BoxSize? filterBySize, bool? sortByAge);
 }
